3247422b010cf98fae50fe757b837a34a0f1aaad
Mike Davis <dodgymike@gmail.com>
2016-03-01 09:35:17 +0200

    threaded ook decoder and jammer

--- ook-decoder/pipe-based-ook-decoder.c
+++ ook-decoder/pipe-based-ook-decoder.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <pthread.h>
+
+float jam_data() {
+  float rand_data = rand();
+  rand_data /= (float)RAND_MAX;
+
+  rand_data *= 50;
+
+  return rand_data;
+}
 
 void float_to_buf(float in, unsigned char* buf) {
   unsigned char* in_bytes = (unsigned char*)&in;
@@ -13,30 +23,67 @@ void float_to_buf(float in, unsigned char* buf) {
 void jam(int jam_on, FILE* jam_file) {
   unsigned char buf[4];
 
-/*
-      float_to_buf(row_i[write_index], buffer);
-      fwrite(buffer, 4, 1, output_file);
-*/
-
-  float jam_i = jam_on ? 0xffff : 0x0000;
+  float jam_i = jam_on ? jam_data() : 0x0000;
   float_to_buf(jam_i, buf);
   fwrite(buf, 4, 1, jam_file);
-  float jam_q = 0xffff;
+  float jam_q = jam_on ? jam_data() : 0x0000;
   float_to_buf(jam_q, buf);
   fwrite(buf, 4, 1, jam_file);
 
-  fflush(jam_file);
-
-//size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+//  fflush(jam_file);
 }
 
-int main(int argc, char* argv[]) {
-  char* input_filename = argv[1];
-  char* jam_filename = argv[2];
-  float raw_bit_rate = atoi(argv[3]);
+struct jam_args {
+  FILE* jam_file;
+  int* jam_on;
+  pthread_mutex_t* jam_lock;
+};
 
-  FILE* jam_file = fopen(jam_filename, "w");
-  FILE* input_file = fopen(input_filename, "r");
+void* jam_loop(void* arg) {
+  struct jam_args* args = (struct jam_args*)arg;
+
+  FILE* jam_file = args->jam_file;
+  int* jam_on = args->jam_on;
+  pthread_mutex_t* jam_lock = args->jam_lock;
+
+  int local_jam_on = 0;
+  while(1) {
+      pthread_mutex_lock(jam_lock);
+        int local_jam_on = *jam_on;
+      pthread_mutex_unlock(jam_lock);
+/*
+      if(local_jam_on) {
+        printf("JAM LOOP JAMMING\n");
+      }
+*/
+
+    jam(local_jam_on, jam_file);
+
+/*
+      for(int i = 0; i < 250000; i++) {
+        jam(1, jam_file);
+      }
+      for(int i = 0; i < 250000; i++) {
+        jam(0, jam_file);
+      }
+*/  
+  }
+}
+
+struct ook_args {
+  FILE* ook_file;
+  float* raw_bit_rate;
+  int* jam_on;
+  pthread_mutex_t* jam_lock;
+};
+
+void* ook_loop(void* arg) {
+  struct ook_args* args = (struct ook_args*)arg;
+
+  FILE* ook_file = args->ook_file;
+  float raw_bit_rate = *(args->raw_bit_rate);
+  int* jam_on = args->jam_on;
+  pthread_mutex_t* jam_lock = args->jam_lock;
 
   float bit_rate = 0;
   int bit_count = 0;
@@ -50,16 +97,24 @@ int main(int argc, char* argv[]) {
   int symbol_bits[max_symbol_bit_count];
   int symbol_bit_index = 0;
 
-  int jam_on = 0;
-
   while(1) {
     int one_count = 0;
     while(cur_bit == 1) {
-      jam(jam_on, jam_file);
-      fread(&cur_bit, 1, 1, input_file);
+      fread(&cur_bit, 1, 1, ook_file);
       one_count++;
+        pthread_mutex_lock(jam_lock);
+        if(*jam_on > 0) {
+          (*jam_on)--;
+        }
+        pthread_mutex_unlock(jam_lock);
     }
 //    printf("one count (%d)\n", one_count);
+/*
+        pthread_mutex_lock(jam_lock);
+          printf("JAM ON!\n");
+          *jam_on = 1;
+        pthread_mutex_unlock(jam_lock);
+*/
 
     // end of byte
     if((decoded_bit_count > 0) && (one_count == 0)) {
@@ -72,7 +127,10 @@ int main(int argc, char* argv[]) {
         }
         printf("\n");
       } else if(symbol_bit_index == 13) {
-        jam_on = 1;
+        pthread_mutex_lock(jam_lock);
+          printf("JAM ON!\n");
+          *jam_on = 50000;
+        pthread_mutex_unlock(jam_lock);
       }
 
       symbol_bit_index = 0;
@@ -89,9 +147,15 @@ int main(int argc, char* argv[]) {
         break;
       }
 
-      jam(jam_on, jam_file);
-      fread(&cur_bit, 1, 1, input_file);
+      fread(&cur_bit, 1, 1, ook_file);
       zero_count++;
+        pthread_mutex_lock(jam_lock);
+        if(*jam_on > 0) {
+          (*jam_on)--;
+        }
+        pthread_mutex_unlock(jam_lock);
+/*
+*/
     }
 
 //    printf("zero count (%d)\n", zero_count);
@@ -104,9 +168,14 @@ int main(int argc, char* argv[]) {
     int on_off = (on_off_ratio > 0.6f);
     int dead_space = (on_off_ratio <= 0.2f);
 
+/*
     if(dead_space) {
-      jam_on = 0;
+        pthread_mutex_lock(jam_lock);
+          //printf("JAM OFF\n");
+          (*jam_on)--;
+        pthread_mutex_unlock(jam_lock);
     }
+*/
 //    printf("on_off_ratio (%f) on_off (%d) dead_space (%d)\n", on_off_ratio, on_off, dead_space);
 
 /*
@@ -118,11 +187,48 @@ int main(int argc, char* argv[]) {
     symbol_bits[symbol_bit_index++] = on_off;
 
     if(symbol_bit_index >= max_symbol_bit_count) {
-      printf("WTF? too many bits in the symbol\n");
-      exit(-1);
+      //printf("WTF? too many bits in the symbol\n");
+      //exit(-1);
+      symbol_bit_index = 0;
     }
     
     decoded_bit_count++;
   }
+}
+
+int main(int argc, char* argv[]) {
+  char* input_filename = argv[1];
+  char* jam_filename = argv[2];
+  float raw_bit_rate = atoi(argv[3]);
+
+  FILE* jam_file = fopen(jam_filename, "w");
+  FILE* ook_file = fopen(input_filename, "r");
+
+  pthread_mutex_t jam_lock;
+  pthread_mutex_init(&jam_lock, NULL);
+
+  int jam_on = 0;
+
+  pthread_t ook_thread;
+  pthread_t jam_thread;
+
+  struct ook_args oargs;
+  oargs.ook_file = ook_file;
+  oargs.raw_bit_rate = &raw_bit_rate;
+  oargs.jam_on = &jam_on;
+  oargs.jam_lock = &jam_lock;
+  pthread_create(&ook_thread, NULL, ook_loop, &oargs);
+
+  struct jam_args jargs;
+  jargs.jam_file = jam_file;
+  jargs.jam_on = &jam_on;
+  jargs.jam_lock = &jam_lock;
+  pthread_create(&jam_thread, NULL, jam_loop, &jargs);
+
+  while(1) {
+    sleep(1000);
+  }
+
+  return 0;
 }
 
